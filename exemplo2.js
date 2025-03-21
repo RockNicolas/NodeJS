ac585e123eefe83208c69fb7c15c7dec65ec72ac
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-03-21 15:46:03 -0300

    Ciração de variavel de routes

--- exemplo2.js
+++ exemplo2.js
@@ -1,10 +1,24 @@
 var http = require("http");
 var url = require("url");
+var routes = require("routres")
+
+var routes = function(pagina){
+    var exist = fs.existsSync(pagina + ".html");
+
+    if(exist){
+        pagina + '.html';
+    } else {
+        "error_404.htlm"
+    }
+};
 
 var server = http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    fs.readFile('Bem vindo.html', function (error,arquivo){
+    var addresPath = url.parse(req.url).pathname;
+    var page = routes(addresPath);
+
+    fs.readFile(page, function (error,arquivo){
         if(error){
             res.end("ERROR")
         } else {
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var http = require("http");
 var url = require("url");
+var fs = require("fs")
 
+var routes = function(pagina){
+    var exist = fs.existsSync(pagina + ".html");
+
+    if(exist){
+        pagina + '.html';
+    } else {
+        "error_404.htlm"
+    }
+};
 var server = http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
+    var page = routes();
     var retorno = fs.readFileSync("bem vindo")
     
     res.end(retorno);
